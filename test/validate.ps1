628040ab69bd8a578510253e73334c381ccd306b
Nephin, John (PSPC GCPS) <John.Nephin@tpsgc-pwgsc.gc.ca>
2019-05-16 16:53:07 -0400

    Update validation

--- test/validate.ps1
+++ test/validate.ps1
@@ -70,9 +70,9 @@ if (-not $doNotCleanup) {
 # Start the deployment
 Write-Host "Starting $templateLibraryName dependancies deployment...";
 
-New-AzureRmDeployment -Location $Location -Name "Deploy-$templateLibraryName-Template-Infrastructure-Dependancies" -TemplateUri "https://raw.githubusercontent.com/canada-ca-azure-templates/masterdeploy/20190514/template/masterdeploysub.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\parameters\masterdeploysub.parameters.json") -baseParametersURL $baseParametersURL -Verbose;
+New-AzureRmDeployment -Location $Location -Name "Deploy-$templateLibraryName-Dependancies" -TemplateUri "https://raw.githubusercontent.com/canada-ca-azure-templates/masterdeploy/20190514/template/masterdeploysub.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\parameters\masterdeploysub.parameters.json") -baseParametersURL $baseParametersURL -Verbose;
 
-$provisionningState = (Get-AzureRmDeployment -Name "Deploy-$templateLibraryName-Template-Infrastructure-Dependancies").ProvisioningState
+$provisionningState = (Get-AzureRmDeployment -Name "Deploy-$templateLibraryName-Dependancies").ProvisioningState
 
 if ($provisionningState -eq "Failed") {
     Write-Host "One of the jobs was not successfully created... exiting..."
