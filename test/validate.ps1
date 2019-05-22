c4bde63bbed7d92b4ae2ee43fd8520a0416f59b5
Nephin, John (PSPC GCPS) <John.Nephin@tpsgc-pwgsc.gc.ca>
2019-05-22 13:54:23 -0400

    Update validation

--- test/validate.ps1
+++ test/validate.ps1
@@ -72,7 +72,7 @@ if (-not $doNotDeployPreReq) {
     Write-Host "Starting $templateLibraryName dependancies deployment...";
 
     New-AzureRmDeployment -Location $Location -Name "Deploy-$templateLibraryName-Dependancies-01" -TemplateUri "https://raw.githubusercontent.com/canada-ca-azure-templates/masterdeploy/20190514/template/masterdeploysubrg.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\parameters\masterdeploysub-01.parameters.json") -baseParametersURL $baseParametersURL -Verbose;
-    New-AzureRmDeployment -Location $Location -Name "Deploy-$templateLibraryName-Dependancies-02" -TemplateUri "https://raw.githubusercontent.com/canada-ca-azure-templates/masterdeploy/20190514/template/masterdeployrg.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\parameters\masterdeploysub-02.parameters.json") -baseParametersURL $baseParametersURL -Verbose;
+    New-AzureRmDeployment -Location $Location -Name "Deploy-$templateLibraryName-Dependancies-02" -TemplateUri "https://raw.githubusercontent.com/canada-ca-azure-templates/masterdeploy/20190514/template/masterdeploysubrg.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\parameters\masterdeploysub-02.parameters.json") -baseParametersURL $baseParametersURL -Verbose;
 
     $provisionningState = (Get-AzureRmDeployment -Name "Deploy-$templateLibraryName-Dependancies-01").ProvisioningState
     $provisionningState2 = (Get-AzureRmDeployment -Name "Deploy-$templateLibraryName-Dependancies-02").ProvisioningState
